f6d8d8c4bad15ce2e0f404f0f64d347d7f3a43e7
Marie-Laure Sin <marie-laure.sin@algolia.com>
2021-04-15 10:37:09 +0200

    nested for loops

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module rampup
 
 go 1.15
+
+require github.com/davecgh/go-spew v1.1.1
--- main.go
+++ main.go
@@ -1,15 +1,32 @@
 package main
 
+import (
+	"fmt"
+
+	"github.com/davecgh/go-spew/spew"
+)
+
 // 1. Get list of URLS
-// 2. 10x in a row, call each URL of the list an get the duration, reachability, status code of each call
+// 2. 10x in a row, call each URL of the list and get the duration, reachability, status code of each call
 // 3. Try n times in case of error for each URL
 // 4. Display metrics on stdout
 
 func main() {
-	println("Hello, World")
+	urls := getURLList()
+	spew.Dump(urls)
+	for k := 0; k < 10; k++ {
+		for _, url := range urls {
+			fmt.Println(url)
+
+		}
+	}
 }
 
 func getURLList() []string {
 	// FIXME: implement
-	return nil
+	return []string{"https://www.algolia.com",
+		"https://d85-usw-1.algolia.net/1/isalive",
+		"https://d85-usw-2.algolia.net/1/isalive",
+		"https://d85-usw-3.algolia.net/1/isalive",
+	}
 }
