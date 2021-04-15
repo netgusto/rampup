5a7eda796daf4eb193c06aa55cab7d0ccd621fef
Jerome Schneider <jerome.schneider@algolia.com>
2021-04-15 10:21:16 +0200

    Empty shell

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+// 1. Get list of URLS
+// 2. 10x in a row, call each URL of the list an get the duration, reachability, status code of each call
+// 3. Try n times in case of error for each URL
+// 4. Display metrics on stdout
+
+func main() {
+	println("Hello, World")
+}
+
+func getURLList() []string {
+	// FIXME: implement
+	return nil
+}
