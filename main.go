80f6cada35d0394d66d9369699284fe7326f9a38
Jerome Schneider <jerome.schneider@algolia.com>
2021-04-15 15:13:23 +0200

    03-test Implemented IT tests

--- main.go
+++ main.go
@@ -2,10 +2,9 @@ package main
 
 import (
 	"fmt"
+
 	"github.com/davecgh/go-spew/spew"
-	"net/http"
 	// "strconv"
-	"time"
 )
 
 // 1. Get list of URLS
@@ -15,43 +14,15 @@ import (
 
 func main() {
 	urls := getURLList()
-	spew.Dump(urls)
+
 	for k := 0; k < 10; k++ {
 		for _, url := range urls {
 			fmt.Println(url)
 
 			measure := measureURL(url, 3)
 			spew.Dump(measure)
-			// fmt.Printf("Status code is %s, duration is %s and also err is %v (at try %d)\n", statusCodeStr, d, err, try)
-			panic("")
 		}
 	}
-}
-
-type measureResult struct  {
-	statusCode *int
-	retries int
-	duration time.Duration
-	err error
-}
-
-func measureURL(url string, maxRetries int) measureResult {
-	var statusCode *int
-	var retries int
-	var duration time.Duration
-	var err error
-	for retries = 0; retries <= maxRetries; retries++ {
-		t1 := time.Now()
-		var resp *http.Response
-		resp, err = http.Get(url)
-		duration = time.Since(t1)
-		if err == nil {
-			statusCode = &resp.StatusCode
-			break
-		}
-		time.Sleep(1 * time.Second)
-	}
-	return measureResult{statusCode: statusCode, retries: retries - 1, duration: duration, err: err}
 }
 
 func getURLList() []string {
